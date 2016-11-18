6714630e1d59b42ffa2466a2905e841354a1d440
Soloman Weng <soloman1124@gmail.com>
2016-11-19 01:11:50 +1000

    Fix elixir install

--- install/elixir.sh
+++ install/elixir.sh
@@ -5,8 +5,12 @@ source ./buildconfig.sh
 echo /root > /etc/container_environment/HOME
 
 apt-key adv --fetch-keys http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc \
+  && apt-get update \
   && apt-get install -y esl-erlang
 
 wget -q https://github.com/elixir-lang/elixir/releases/download/v1.3.4/Precompiled.zip \
   && unzip -dnq /usr/local Precompiled.zip \
   && rm Precompiled.zip
+
+/usr/local/bin/mix local.hex --force \
+  && /usr/local/bin/mix local.rebar --force
