6576ea25bf4dba80a1fe16894c36552b3a2c97b1
Soloman Weng <soloman1124@gmail.com>
2016-11-19 01:38:14 +1000

    Another try

--- install/elixir.sh
+++ install/elixir.sh
@@ -2,7 +2,8 @@
 set -e
 source ./buildconfig.sh
 
-echo /root > /etc/container_environment/HOME
+export HOME=/root
+echo $HOME > /etc/container_environment/HOME
 
 echo "deb http://packages.erlang-solutions.com/ubuntu trusty contrib" >> /etc/apt/sources.list \
   && apt-key adv --fetch-keys http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc \
@@ -10,7 +11,7 @@ echo "deb http://packages.erlang-solutions.com/ubuntu trusty contrib" >> /etc/ap
   && apt-get install -y esl-erlang
 
 wget -q https://github.com/elixir-lang/elixir/releases/download/v1.3.4/Precompiled.zip \
-  && unzip -dnq /usr/local Precompiled.zip \
+  && unzip -nq -d /usr/local Precompiled.zip \
   && rm Precompiled.zip
 
 /usr/local/bin/mix local.hex --force \
